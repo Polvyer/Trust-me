2a2b9b711e9f73ebfc8dfdf7f9f57027c73ceffe
William Ocampo <55007870+Polvyer@users.noreply.github.com>
2020-04-06 14:17:24 -0500

    Add Trust Me Again

---
+++ Trust me/Assets/Inventory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject inventory;
+    public GameObject slotHolder;
+    private bool inventoryEnabled;
+
+    private int slots;
+    private Transform[] slot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slots = slotHolder.transform.childCount;
+        slot = new Transform[slots];
+        DetectInventorySlots();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryEnabled = !inventoryEnabled;
+        }
+
+        if (inventoryEnabled)
+        {
+            inventory.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            inventory.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public void DetectInventorySlots()
+    {
+        for(int i = 0; i < slots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i);
+            print(slot[i].name);
+        }
+    }
+}
