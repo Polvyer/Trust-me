886a2f7a083c28d0ac2d30c4eeaba10d58961b9e
William Ocampo <55007870+Polvyer@users.noreply.github.com>
2020-04-07 15:06:27 -0500

    Item Script
    Adds item to inventory

--- Trust me/Assets/Scripts/Slot.cs
+++ Trust me/Assets/Scripts/Slot.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public bool hovered;
-    private bool used;
+    public bool empty;
 
-    private GameObject item;
-    private Texture itemIcon;
+    public GameObject item;
+    public Texture itemIcon;
 
     // Start is called before the first frame update
     void Start()
@@ -20,16 +21,38 @@ public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     // Update is called once per frame
     void Update()
     {
-        
+        if (item)
+        {
+            empty = false;
+            itemIcon = item.GetComponent<Item>().icon;
+            this.GetComponent<RawImage>().texture = itemIcon;
+        }
+        else
+        {
+            empty = true;
+        }
     }
 
+    // Runs when the mouse cursor is hovering over the specific inventory slot
     public void OnPointerEnter(PointerEventData eventData)
     {
         hovered = true;
     }
 
+    // Runs when the mouse cursor is no longer hovering over the specific inventory slot
     public void OnPointerExit(PointerEventData eventData)
     {
         hovered = false;
+    }
+
+    // Runs when the mouse cursor clicks on the specific inventory slot
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (item)
+        {
+            Item thisItem = item.GetComponent<Item>();
+
+            // checking for item type
+        }
     }
 }
