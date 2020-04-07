4475e0fd06375fa80a07fc935d10666099a33e60
ginagerace <ggmari99@gmail.com>
2020-04-06 20:23:59 -0500

    added cat model

---
+++ Trust me/Assets/AI-Mecanim/Calico Cat/Model/CatWalk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatWalk : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("SpeedUp");
+        }
+    }
+}
+
