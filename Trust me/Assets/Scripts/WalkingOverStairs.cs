82caf861adb297b061a7b45b596cf4b4c6bbb20b
William Ocampo <55007870+Polvyer@users.noreply.github.com>
2020-04-07 22:17:43 -0500

    Added WalkingOverStairs Script
    Makes sound when the player walks over stairs and stops the sound when the player gets out of it

---
+++ Trust me/Assets/Scripts/WalkingOverStairs.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingOverStairs : MonoBehaviour
+{
+    public AudioClip sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = sound;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
