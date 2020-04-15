b85c91ea3192b0bdb70410a14bba6395048f788b
William Ocampo <55007870+Polvyer@users.noreply.github.com>
2020-04-15 14:19:41 -0500

    Revert "Revert "added main menu and pause menu in game""
    This reverts commit c66987db2c606b941e24b27603bde87b2c8c7153.

---
+++ Trust me/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        ResumeGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }//end of Update
+
+    public void ResumeGame()
+    {
+        // Resume time and hide the pause menu UI
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    void PauseGame()
+    {
+        // Freeze time and bring up pause menu UI
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
