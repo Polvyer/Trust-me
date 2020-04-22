6de4ae497b01f477e5c65c9139bf7d7644051abf
Darell <darellescofe@gmail.com>
2020-04-22 03:09:04 -0500

    fixed keypad door/ added glow around pad

---
+++ Trust me/Assets/OpenLockedDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLockedDoor : MonoBehaviour
+{
+    
+    //public GameObject door;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        //GetComponent<AudioSource>().playOnAwake = false;
+    }
+
+    void Update()
+    {
+        //Debug.Log(GameObject.Find("KeypadCanvas").GetComponent<Keypad>().unlockDoor);
+        if(GameObject.Find("KeypadCanvas").GetComponent<Keypad>().unlockDoor){
+            //destroy door and play sound effect
+            GetComponent<AudioSource>().Play();
+            Destroy(gameObject);
+        }
+    }
+}
